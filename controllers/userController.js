5c8b3a1f8220e0b840eee22e502305cf7992d85b
mahm0udnasr <mahmoudnasrlite@gmail.com>
2025-09-29 10:08:14 +0300

    setup user controller file

---
+++ controllers/userController.js
@@ -0,0 +1,39 @@
+export const getUsers = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message || "Server Error",
+    });
+  }
+};
+
+export const createUser = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message || "Server Error",
+    });
+  }
+};
+
+export const editUser = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message || "Server Error",
+    });
+  }
+};
+
+export const deleteUser = async (req, res) => {
+  try {
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: error.message || "Server Error",
+    });
+  }
+};
